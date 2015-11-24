be081f94f6828fdcd1fdd7ded1e20ef6fcf0a106
Lars Kotthoff <larsko@cs.ubc.ca>
2015-11-23 20:48:55 -0800

    dynamic projection parameters and circles on top of boundaries

--- js/main.js
+++ js/main.js
@@ -20,10 +20,8 @@ var zoom = d3.behavior.zoom()
 
 //Define map projection
 var projection = d3.geo.mercator()
-.center([-77.5, 38.4])
-.translate([ w / 2, h / 2 ])
-.scale([w * 11.5]);
-//      .rotate([77.56, 0]);
+.translate([0, 0])
+.scale(1);
 
 //Define path generator
 var path = d3.geo.path()
@@ -42,62 +40,15 @@ var colors = d3.scale.category10();
 
 d3.csv("HB980-vote-2014.csv", function (data) {
 
-  //Create a circle for each city
-  svg.selectAll("circle")
-    .data(data)
-    .enter()
-    .append("circle")
-    .attr("cx", function (d) {
-      //[0] returns the first coordinate (x) of the projected value
-      return projection([d.longitude, d.latitude])[0];
-    })
-.attr("cy", function (d) {
-  //[1] returns the second coordinate (y) of the projected value
-  return projection([d.longitude, d.latitude])[1];
-  })
-.attr("r", 3.4)
-.on("mouseover", function () {
-  return tooltip.style("visibility", "visible");
-  })
-.on("mousemove", function (d, i) {
-
-  var ttip = "<strong>"
-  + d.full_name
-  + "</strong>"
-  + " ("
-  + d.party.charAt(0)
-  + ")<br />District "
-  + d.indexmatch
-  + "<br />" + d.hb0980;
-  return tooltip
-  .style("top", (d3.event.pageY - 10) + "px")
-  .style("left", (d3.event.pageX + 10) + "px")
-  .html(ttip)
-  })
-.on("mouseout", function () {
-  return tooltip.style("visibility", "hidden");
-  })
-.style("fill", "yellow")
-.style("opacity", 0.84)
-.style("fill", function (d) {
-
-  //Get data value
-  var value = d.hb0980;
-
-  if (value) {
-  //If value exists…
-  return colors(value);
-  } else {
-  //If value is undefined…
-  return "#bcbcbc";
-  }
-
-})
-
-});
-//Load in GeoJSON data
-//  d3.json("../data/countries/mapshaper_output.json", function(json) {
   d3.json("md-house-boundary.json", function (json) {
+    // Compute right scale and translation
+    var b = path.bounds(json),
+      s = .9 / Math.max((b[1][0] - b[0][0]) / w, (b[1][1] - b[0][1]) / h),
+      t = [(w - s * (b[1][0] + b[0][0])) / 2, (h/20 - s * b[0][1])];
+    projection
+      .scale(s)
+      .translate(t);
+
     //Bind data and create one path per GeoJSON feature
     svg.selectAll("path")
       .data(json.geometries)
@@ -105,8 +56,61 @@ d3.csv("HB980-vote-2014.csv", function (data) {
       .append("path")
       .attr("d", path)
       .attr("fill", "none")
-      .attr("stroke", "#bbb")
+      .attr("stroke", "#bbb");
+
+      //Create a circle for each city
+      svg.selectAll("circle")
+        .data(data)
+        .enter()
+        .append("circle")
+        .attr("cx", function (d) {
+          //[0] returns the first coordinate (x) of the projected value
+          return projection([d.longitude, d.latitude])[0];
+        })
+    .attr("cy", function (d) {
+      //[1] returns the second coordinate (y) of the projected value
+      return projection([d.longitude, d.latitude])[1];
+      })
+    .attr("r", 3.4)
+    .on("mouseover", function () {
+      return tooltip.style("visibility", "visible");
+      })
+    .on("mousemove", function (d, i) {
+
+      var ttip = "<strong>"
+      + d.full_name
+      + "</strong>"
+      + " ("
+      + d.party.charAt(0)
+      + ")<br />District "
+      + d.indexmatch
+      + "<br />" + d.hb0980;
+      return tooltip
+      .style("top", (d3.event.pageY - 10) + "px")
+      .style("left", (d3.event.pageX + 10) + "px")
+      .html(ttip)
+      })
+    .on("mouseout", function () {
+      return tooltip.style("visibility", "hidden");
+      })
+    .style("fill", "yellow")
+    .style("opacity", 0.84)
+    .style("fill", function (d) {
+
+      //Get data value
+      var value = d.hb0980;
+
+      if (value) {
+      //If value exists…
+      return colors(value);
+      } else {
+      //If value is undefined…
+      return "#bcbcbc";
+      }
+
+    });
   });
+});
 
 function zoomed() {
   g.style("stroke-width", 1.5 / d3.event.scale + "px");
